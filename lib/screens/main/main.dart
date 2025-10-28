738bc1e443e6736b1b5af533ac96d1032151a43a
minwoo1119 <minwoo1119@naver.com>
2025-10-28 19:38:21 +0900

    feat: 하단 네비게이션 바를 위한 main 파일 생성 ( #6 )

--- lib/screens/main/main.dart
+++ lib/screens/main/main.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:geumpumta/screens/home/home.dart';
 
-class MainScreen extends StatelessWidget {
+import '../ranking/ranking.dart';
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    // StatsScreen(),
+    RankingScreen(),
+    // MoreScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'This is Main Screen',
-            style: TextStyle(color: Colors.black, fontSize: 20),
-          ),
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: '통계'),
+          BottomNavigationBarItem(icon: Icon(Icons.emoji_events), label: '랭킹'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: '더보기'),
+        ],
       ),
     );
   }

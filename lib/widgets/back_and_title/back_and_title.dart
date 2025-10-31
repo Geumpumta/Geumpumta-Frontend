3867fac35091af1c69c8f835ab18231031c71927
minwoo1119 <minwoo1119@naver.com>
2025-10-31 19:59:31 +0900

    fix: SafeArea 제거( #9 )

--- lib/widgets/back_and_title/back_and_title.dart
+++ lib/widgets/back_and_title/back_and_title.dart
@@ -7,31 +7,29 @@ class BackAndTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-        width: double.infinity,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Text(
-              title,
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
-            ),
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
+          ),
 
-            Positioned(
-              left: 0,
-              child: GestureDetector(
-                onTap: () => Navigator.pop(context),
-                child: const Icon(
-                  Icons.arrow_back,
-                  size: 28,
-                  color: Color(0xFF898989),
-                ),
+          Positioned(
+            left: 0,
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(
+                Icons.arrow_back,
+                size: 28,
+                color: Color(0xFF898989),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

0436e234b9584ad5a133a61ea5081dfa3e3f6225
minwoo1119 <minwoo1119@naver.com>
2025-11-02 17:08:07 +0900

    feat: 기간 정보(월간, 주간, 일간) 정보 입력받아 날짜 변경하는 periodPicker 생성 ( #9 )

---
+++ lib/screens/ranking/widgets/custom_period_picker.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:geumpumta/screens/ranking/ranking.dart';
+
+class CustomPeriodPicker extends StatefulWidget {
+  const CustomPeriodPicker({
+    super.key,
+    required this.option,
+    required this.selectedDate,
+    required this.onSelect,
+  });
+
+  final PeriodOption option;
+  final DateTime selectedDate;
+  final ValueChanged<DateTime> onSelect;
+
+  @override
+  State<CustomPeriodPicker> createState() => _CustomPeriodPickerState();
+}
+
+class _CustomPeriodPickerState extends State<CustomPeriodPicker> {
+  @override
+  Widget build(BuildContext context) {
+    int getWeekOfMonth(DateTime date) {
+      DateTime firstDayOfMonth = DateTime(date.year, date.month, 1);
+      int firstWeekday = firstDayOfMonth.weekday;
+
+      int dayOfMonth = date.day;
+
+      return ((dayOfMonth + firstWeekday - 1) / 7).ceil();
+    }
+
+    String formattedViewText(DateTime date) {
+      if (widget.option == PeriodOption.daily)
+        return '${widget.selectedDate.year}.${widget.selectedDate.month}.${widget.selectedDate.day}';
+      else if (widget.option == PeriodOption.weekly)
+        return '${widget.selectedDate.month}월 ${getWeekOfMonth(widget.selectedDate)}주차';
+      else
+        return '${widget.selectedDate.year}.${widget.selectedDate.month}';
+    }
+
+
+
+    void decreaseDate() {
+      if (widget.option == PeriodOption.daily)
+        widget.onSelect(widget.selectedDate.subtract(const Duration(days: 1)));
+      else if (widget.option == PeriodOption.weekly)
+        widget.onSelect(widget.selectedDate.subtract(const Duration(days: 7)));
+      else
+        widget.onSelect(
+          DateTime(
+            widget.selectedDate.year,
+            widget.selectedDate.month - 1,
+            1,
+          ),
+        );
+    }
+
+    void increaseDate() {
+      if (widget.option == PeriodOption.daily)
+        widget.onSelect(widget.selectedDate.add(const Duration(days: 1)));
+      else if (widget.option == PeriodOption.weekly)
+        widget.onSelect(widget.selectedDate.add(const Duration(days: 7)));
+      else
+        widget.onSelect(
+          DateTime(
+            widget.selectedDate.year,
+            widget.selectedDate.month + 1,
+            1,
+          ),
+        );
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {
+            decreaseDate();
+          },
+          icon: Icon(Icons.chevron_left),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            formattedViewText(widget.selectedDate),
+            style: TextStyle(
+              color: Color(0xFF000000),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            increaseDate();
+          },
+          icon: Icon(Icons.chevron_right),
+        ),
+      ],
+    );
+  }
+}

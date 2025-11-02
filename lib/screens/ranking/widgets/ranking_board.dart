7f0eff4719713ba35bbbbf188bcd2ef39059cc85
minwoo1119 <minwoo1119@naver.com>
2025-11-02 17:17:41 +0900

    feat: 사각 버튼 및 날짜 picker 적용 ranking_board 위젯 생성 ( #9 )

--- lib/screens/ranking/widgets/ranking_board.dart
+++ lib/screens/ranking/widgets/ranking_board.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:geumpumta/screens/ranking/ranking.dart';
+import 'package:geumpumta/screens/ranking/widgets/custom_period_picker.dart';
 import 'package:geumpumta/screens/ranking/widgets/square_option_button.dart';
 
+enum GroupOption { personal, department }
+
+extension GroupOptionExtension on GroupOption {
+  String get koreanName {
+    switch (this) {
+      case GroupOption.personal:
+        return '개인';
+      case GroupOption.department:
+        return '학과';
+    }
+  }
+}
+
+// 이 위젯에서 랭킹 데이터 받아와서 관리해야할듯. ranking.dart에는 지금 너무 상태가 많음
 class RankingBoard extends StatefulWidget {
-  const RankingBoard({super.key});
+  const RankingBoard({super.key, required this.periodOption});
+
+  final PeriodOption periodOption;
 
   @override
   State<RankingBoard> createState() => _RankingBoardState();
 }
 
 class _RankingBoardState extends State<RankingBoard> {
+  DateTime _selectedPeriodDate = DateTime.now();
+  GroupOption _selectedGroup = GroupOption.personal;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,8 +44,34 @@ class _RankingBoardState extends State<RankingBoard> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SquareOptionButton(text: '전체', isActive: true),
-                SquareOptionButton(text: '학과', isActive: false),
+                SquareOptionButton(
+                  text: '개인',
+                  isActive: _selectedGroup==GroupOption.personal,
+                  onSelect: () {
+                    setState(() {
+                      _selectedGroup = GroupOption.personal;
+                    });
+                  },
+                ),
+                SquareOptionButton(
+                  text: '학과',
+                    isActive: _selectedGroup==GroupOption.department,                  onSelect: () {
+                    setState(() {
+                      _selectedGroup = GroupOption.department;
+                    });
+                  },
+                ),
+                Expanded(
+                  child: CustomPeriodPicker(
+                    option: widget.periodOption,
+                    selectedDate: _selectedPeriodDate,
+                    onSelect: (d) {
+                      setState(() {
+                        _selectedPeriodDate = d;
+                      });
+                    },
+                  ),
+                ),
               ],
             ),
           ),

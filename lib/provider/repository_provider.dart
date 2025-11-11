90ef07c088d33bec09d364bf073b55a1f0b05261
minwoo1119 <minwoo1119@naver.com>
2025-11-11 22:30:05 +0900

    feat: 이메일 인증 repository 생성 ( #15 )

--- lib/provider/repository_provider.dart
+++ lib/provider/repository_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geumpumta/repository/email/email_repository.dart';
 
 import '../repository/user/user_repository.dart';
 import 'api_provider.dart';
@@ -6,4 +7,9 @@ import 'api_provider.dart';
 final userRepositoryProvider = Provider<UserRepository>((ref) {
   final api = ref.watch(userApiProvider);
   return UserRepository(api);
+});
+
+final emailRepositoryProvider = Provider<EmailRepository>((ref){
+  final api = ref.watch(emailApiProvider);
+  return EmailRepository(emailApi: api);
 });

a039e96a98bf60779b698cb385fa651441f72095
minwoo1119 <minwoo1119@naver.com>
2025-11-15 18:46:33 +0900

    fix: 공부 시간 측정 응답 DTO 형식 수정 ( #19 )

--- lib/models/dto/study/start_study_time_request_dto.dart
+++ lib/models/dto/study/start_study_time_request_dto.dart
@@ -1,18 +1,18 @@
 class StartStudyTimeRequestDto {
   final DateTime startTime;
-  final String ssid;
+  final String gatewayIp;
   final String bssid;
 
   StartStudyTimeRequestDto({
     required this.startTime,
-    required this.ssid,
+    required this.gatewayIp,
     required this.bssid,
   });
 
   Map<String, dynamic> toJson() {
     return {
       "startTime": startTime.toIso8601String(),
-      "ssid": ssid,
+      "ssid": gatewayIp,
       "bssid": bssid,
     };
   }

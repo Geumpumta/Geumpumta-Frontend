15327c33adb8304af91f04251899592b5b5e9619
minwoo1119 <minwoo1119@naver.com>
2025-11-15 18:45:50 +0900

    fix: study_viewmodel AsyncValue 형식 제거 ( #19 )

--- lib/viewmodel/study/study_viewmodel.dart
+++ lib/viewmodel/study/study_viewmodel.dart
@@ -1,15 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geumpumta/models/dto/common/common_dto.dart';
 import 'package:geumpumta/models/dto/study/end_study_request_dto.dart';
+import 'package:geumpumta/models/dto/study/get_study_time_response_dto.dart';
 import 'package:geumpumta/models/dto/study/send_heart_beat_request_dto.dart';
+import 'package:geumpumta/models/dto/study/send_heart_beat_response_dto.dart';
 import 'package:geumpumta/models/dto/study/start_study_time_request_dto.dart';
+import 'package:geumpumta/models/dto/study/start_study_time_response_dto.dart';
 import 'package:geumpumta/provider/repository_provider.dart';
 import 'package:geumpumta/repository/study/study_repository.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 
-final studyViewmodelProvider =
-    StateNotifierProvider<StudyViewmodel, AsyncValue<dynamic>>((ref) {
-      final repo = ref.watch(studyRepositoryProvider);
-      return StudyViewmodel(ref, repo);
-    });
+final studyViewmodelProvider = Provider<StudyViewmodel>((ref) {
+  final repo = ref.watch(studyRepositoryProvider);
+  return StudyViewmodel(ref, repo);
+});
+
 
 class StudyViewmodel extends StateNotifier<AsyncValue<dynamic>> {
   final Ref ref;
@@ -17,71 +22,79 @@ class StudyViewmodel extends StateNotifier<AsyncValue<dynamic>> {
 
   StudyViewmodel(this.ref, this.repo):super(const AsyncData(null));
 
-  Future<void> getStudyTime() async {
-    state = const AsyncLoading();
+  Future<GetStudyTimeResponseDto?> getStudyTime() async {
     try {
       final response = await repo.getStudyTime();
       if (!response.success) {
-        state = AsyncError(
-          response.message ?? "알 수 없는 오류",
-          StackTrace.current,
-        );
-        return;
+        throw Exception(response.message);
       }
-      state = AsyncData(response.data);
+      return response;
     } catch (e, st) {
-      state = AsyncError(e, st);
+      print('[StudyViewmodel] 오류 발생: $e\n$st');
     }
+    return null;
   }
 
-  Future<void> startStudyTime(StartStudyTimeRequestDto dto) async {
-    state = const AsyncLoading();
-    try{
+  Future<StartStudyTimeResponseDto?> startStudyTime(StartStudyTimeRequestDto dto) async {
+    try {
       final response = await repo.startStudyTime(dto);
       if (!response.success) {
-        state = AsyncError(
-          response.message ?? "알 수 없는 오류",
-          StackTrace.current,
-        );
-        return;
+        throw Exception(response.message);
       }
-      state = AsyncData(response.data);
-    }catch (e, st) {
-      state = AsyncError(e, st);
+      return response;
+    } catch (e, st) {
+      print('[StudyViewmodel] 오류 발생: $e\n$st');
     }
+    return null;
   }
 
-  Future<void> sendHeartBeat(SendHeartBeatRequestDto dto) async{
+
+  Future<SendHeartBeatResponseDto?> sendHeartBeat(SendHeartBeatRequestDto dto) async{
     try{
       final response = await repo.sendHeartBeat(dto);
       if (!response.success) {
-        state = AsyncError(
-          response.message ?? "알 수 없는 오류",
-          StackTrace.current,
-        );
-        return;
+        throw Exception(response.message);
       }
-      state = AsyncData(response.data);
+      return response;
     }catch (e, st) {
-      state = AsyncError(e, st);
+      print('[StudyViewmodel] 오류 발생: $e\n$st');
     }
+    return null;
   }
 
-  Future<void> endStudyTime(EndStudyRequestDto dto) async {
-    state = const AsyncLoading();
+  Future<CommonDto?> endStudyTime(EndStudyRequestDto dto) async {
     try{
       final response = await repo.endStudyTime(dto);
       if (!response.success) {
-        state = AsyncError(
-          response.message ?? "알 수 없는 오류",
-          StackTrace.current,
-        );
-        return;
+        throw Exception(response.message);
       }
-      state = AsyncData(response.data);
+      return response;
     }catch (e, st) {
-      state = AsyncError(e, st);
+      print('[StudyViewmodel] 오류 발생: $e\n$st');
+    }
+    return null;
+  }
+
+  Future<Map<String, String?>> getWIFIInfo() async {
+    final networkInfo = NetworkInfo();
+
+    try {
+      final bssid = await networkInfo.getWifiBSSID();
+      final gatewayIp = await networkInfo.getWifiGatewayIP();
+      print('bssid : $bssid, gatewayIp : $gatewayIp');
+
+      return {
+        "bssid": bssid,
+        "gatewayIp": gatewayIp,
+      };
+    } catch (e, st) {
+      print("WIFI error: $e\n$st");
+      return {
+        "bssid": null,
+        "gatewayIp": null,
+      };
     }
   }
+
 
 }

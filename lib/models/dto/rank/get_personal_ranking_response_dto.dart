a69f3e71a9f2570abf7d763636781313c27a8ddb
minwoo1119 <minwoo1119@naver.com>
2025-11-16 21:47:49 +0900

    feat: 개인별 랭킹 조회 dto 생성 ( #21 )

---
+++ lib/models/dto/rank/get_personal_ranking_response_dto.dart
@@ -0,0 +1,55 @@
+class PersonalRankingDataItem {
+  final int userId;
+  final int totalMillis;
+  final int rank;
+  final String username;
+
+  PersonalRankingDataItem({
+    required this.userId,
+    required this.totalMillis,
+    required this.rank,
+    required this.username,
+  });
+
+  factory PersonalRankingDataItem.fromJson(Map<String, dynamic> json) =>
+      PersonalRankingDataItem(
+        userId: json['userId'] as int,
+        totalMillis: json['totalMillis'] as int,
+        rank: json['rank'] as int,
+        username: json['username'],
+      );
+}
+
+class PersonalRankingData {
+  final List<PersonalRankingDataItem> topRanks;
+  final PersonalRankingDataItem myRanking;
+
+  PersonalRankingData({required this.topRanks, required this.myRanking});
+
+  factory PersonalRankingData.fromJson(Map<String, dynamic> json) =>
+      PersonalRankingData(
+        topRanks: (json['topRanks'] as List<dynamic>)
+            .map((item) => PersonalRankingDataItem.fromJson(item))
+            .toList(),
+        myRanking: PersonalRankingDataItem.fromJson(json['myRanking']),
+      );
+}
+
+class GetPersonalRankingResponseDto {
+  final bool success;
+  final PersonalRankingData data;
+  final String? message;
+
+  GetPersonalRankingResponseDto({
+    required this.success,
+    required this.data,
+    this.message,
+  });
+
+  factory GetPersonalRankingResponseDto.fromJson(Map<String, dynamic> json) =>
+      GetPersonalRankingResponseDto(
+        success: json['success'] == 'true' || json['success'] == true,
+        data: PersonalRankingData.fromJson(json['data']),
+        message: json['message'],
+      );
+}

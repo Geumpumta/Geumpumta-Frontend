e8093a03f940c097a77c45bfdd935467b5398ab6
minwoo1119 <minwoo1119@naver.com>
2025-11-16 21:47:35 +0900

    feat: 학과별 조회 dto 생성 ( #21 )

---
+++ lib/models/dto/rank/get_department_ranking_response_dto.dart
@@ -0,0 +1,56 @@
+class DepartmentRankingDataItem {
+  final String departmentName;
+  final int totalMillis;
+  final int rank;
+
+  DepartmentRankingDataItem({
+    required this.departmentName,
+    required this.totalMillis,
+    required this.rank,
+  });
+
+  factory DepartmentRankingDataItem.fromJson(Map<String, dynamic> json) =>
+      DepartmentRankingDataItem(
+        departmentName: json['departmentName'],
+        totalMillis: json['totalMillis'] as int,
+        rank: json['rank'] as int,
+      );
+}
+
+class DepartmentRankingData {
+  final List<DepartmentRankingDataItem> topRanks;
+  final DepartmentRankingDataItem myDepartmentRanking;
+
+  DepartmentRankingData({
+    required this.topRanks,
+    required this.myDepartmentRanking,
+  });
+
+  factory DepartmentRankingData.fromJson(Map<String, dynamic> json) =>
+      DepartmentRankingData(
+        topRanks: (json['topRanks'] as List<dynamic>)
+            .map((item) => DepartmentRankingDataItem.fromJson(item))
+            .toList(),
+        myDepartmentRanking:
+        DepartmentRankingDataItem.fromJson(json['myDepartmentRanking']),
+      );
+}
+
+class GetDepartmentRankingResponseDto {
+  final bool success;
+  final DepartmentRankingData data;
+  final String? message;
+
+  GetDepartmentRankingResponseDto({
+    required this.success,
+    required this.data,
+    this.message,
+  });
+
+  factory GetDepartmentRankingResponseDto.fromJson(Map<String, dynamic> json) =>
+      GetDepartmentRankingResponseDto(
+        success: json['success'] == 'true' || json['success'] == true,
+        data: DepartmentRankingData.fromJson(json['data']),
+        message: json['message'],
+      );
+}

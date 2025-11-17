0e095599e27d542bb13297300d66b74bd2176845
minwoo1119 <minwoo1119@naver.com>
2025-11-17 10:35:35 +0900

    feat: 랭킹 viewmodel 생성 ( #21 )

---
+++ lib/viewmodel/rank/rank_department_viewmodel.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geumpumta/provider/repository_provider.dart';
+import 'package:geumpumta/repository/rank/rank_repository.dart';
+import '../../models/dto/rank/get_department_ranking_response_dto.dart';
+
+final rankDepartmentViewModelProvider =
+StateNotifierProvider<
+    RankDepartmentViewmodel,
+    AsyncValue<GetDepartmentRankingResponseDto?>
+>((ref) {
+  final repository = ref.watch(rankRepositoryProvider);
+  return RankDepartmentViewmodel(ref, repository);
+});
+
+class RankDepartmentViewmodel
+    extends StateNotifier<AsyncValue<GetDepartmentRankingResponseDto?>> {
+  final Ref ref;
+  final RankRepository repository;
+
+  RankDepartmentViewmodel(this.ref, this.repository):super(const AsyncData(null));
+
+  Future<void> getWeeklyDepartmentRanking(DateTime date) async{
+    state = AsyncLoading();
+    try{
+      final response = await repository.getWeeklyDepartmentRanking(date);
+      state = AsyncData(response);
+    }catch(e,st){
+      print('[RankDepartmentViewmodel] 오류 발생: $e\n$st');
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> getDailyDepartmentRanking(DateTime date) async{
+    state = AsyncLoading();
+    try{
+      final response = await repository.getDailyDepartmentRanking(date);
+      state = AsyncData(response);
+    }catch(e,st){
+      print('[RankDepartmentViewmodel] 오류 발생: $e\n$st');
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> getMonthlyDepartmentRanking(DateTime date) async{
+    state = AsyncLoading();
+    try{
+      final response = await repository.getMonthlyDepartmentRanking(date);
+      state = AsyncData(response);
+    }catch(e,st){
+      print('[RankDepartmentViewmodel] 오류 발생: $e\n$st');
+      state = AsyncError(e, st);
+    }
+  }
+}

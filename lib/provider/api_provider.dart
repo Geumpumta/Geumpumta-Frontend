df9c974d4a5c006ca0caf34a50879538bf16eb37
Pangmin2 <totoro7378@naver.com>
2025-11-18 22:37:09 +0900

    feat: 새 API Provider 등록 추가 ( #20 , #13 )

--- lib/provider/api_provider.dart
+++ lib/provider/api_provider.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geumpumta/service/retrofit/daily_statistics_api.dart';
 import 'package:geumpumta/service/retrofit/email_api.dart';
+import 'package:geumpumta/service/retrofit/grass_statistics_api.dart';
+import 'package:geumpumta/service/retrofit/monthly_statistics_api.dart';
+import 'package:geumpumta/service/retrofit/profile_api.dart';
 import 'package:geumpumta/service/retrofit/study_api.dart';
+import 'package:geumpumta/service/retrofit/user_api.dart';
+import 'package:geumpumta/service/retrofit/weekly_statistics_api.dart';
 
-import '../service/retrofit/user_api.dart';
 import 'dio_provider.dart';
 
 final userApiProvider = Provider<UserApi>((ref) {
@@ -18,4 +23,29 @@ final emailApiProvider = Provider<EmailApi>((ref){
 final studyApiProvider = Provider<StudyApi>((ref){
   final dio = ref.watch(dioProvider);
   return StudyApi(dio);
+});
+
+final profileApiProvider = Provider<ProfileApi>((ref) {
+  final dio = ref.watch(dioProvider);
+  return ProfileApi(dio);
+});
+
+final weeklyStatisticsApiProvider = Provider<WeeklyStatisticsApi>((ref) {
+  final dio = ref.watch(dioProvider);
+  return WeeklyStatisticsApi(dio);
+});
+
+final monthlyStatisticsApiProvider = Provider<MonthlyStatisticsApi>((ref) {
+  final dio = ref.watch(dioProvider);
+  return MonthlyStatisticsApi(dio);
+});
+
+final dailyStatisticsApiProvider = Provider<DailyStatisticsApi>((ref) {
+  final dio = ref.watch(dioProvider);
+  return DailyStatisticsApi(dio);
+});
+
+final grassStatisticsApiProvider = Provider<GrassStatisticsApi>((ref) {
+  final dio = ref.watch(dioProvider);
+  return GrassStatisticsApi(dio);
 });

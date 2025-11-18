6c10c901528bdef4497b72b464390763fb5185db
minwoo1119 <minwoo1119@naver.com>
2025-11-18 21:39:13 +0900

    fix: department 객체 Parser 추가 ( #21 )

--- lib/models/department.dart
+++ lib/models/department.dart
@@ -83,3 +83,62 @@ extension DepartmentExtension on Department {
     }
   }
 }
+
+extension DepartmentParser on Department {
+  static Department fromKorean(String? name) {
+    if (name == null) return Department.none;
+
+    switch (name) {
+      case '건축공학전공':
+        return Department.architectureEngineering;
+      case '건축학전공':
+        return Department.architecture;
+      case '토목공학전공':
+        return Department.civilEngineering;
+      case '환경공학전공':
+        return Department.environmentalEngineering;
+      case '기계공학전공':
+        return Department.mechanicalEngineering;
+      case '기계시스템공학전공':
+        return Department.mechanicalSystemsEngineering;
+      case '스마트모빌리티전공':
+        return Department.smartMobility;
+      case '산업공학전공':
+        return Department.industrialEngineering;
+      case '수리빅데이터전공':
+        return Department.appliedMathBigData;
+      case '고분자공학전공':
+        return Department.polymerEngineering;
+      case '신소재공학전공':
+        return Department.materialsEngineering;
+      case '반도체시스템전공':
+        return Department.semiconductorSystems;
+      case '전자시스템전공':
+        return Department.electronicSystems;
+      case '소프트웨어전공':
+        return Department.software;
+      case '인공지능공학전공':
+        return Department.artificialIntelligence;
+      case '컴퓨터공학전공':
+        return Department.computerEngineering;
+      case '소재디자인공학전공':
+        return Department.materialsDesignEngineering;
+      case '화학공학전공':
+        return Department.chemicalEngineering;
+      case '화학생명소재전공':
+        return Department.chemicalBioMaterials;
+      case '광시스템공학과':
+        return Department.opticalSystems;
+      case '바이오메디컬공학과':
+        return Department.biomedicalEngineering;
+      case 'IT융합학과':
+        return Department.itConvergence;
+      case '자율전공학부':
+        return Department.liberalMajor;
+      case '경영학과':
+        return Department.businessAdministration;
+      default:
+        return Department.none;
+    }
+  }
+}
